14b6be7b8aeb65b018413859c90dbf3423a2dda8
Tsvetelin91 <cv.dilov@abv.bg>
2023-09-02 14:09:56 +0300

    Creating the Domain project and adding the domain models in it

---
+++ CleanArchitectureNewVersion/Hr.LeaveManagement.Domain/LeaveAllocation.cs
@@ -0,0 +1,17 @@
+﻿using Hr.LeaveManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hr.LeaveManagement.Domain
+{
+    public class LeaveAllocation : BaseDomainEntity
+    {
+        public int NumberDays { get; set; }
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set; }
+    }
+}
